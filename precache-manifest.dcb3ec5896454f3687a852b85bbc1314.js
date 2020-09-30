31937c788b18e2ac5ecbe6248adea8ea287be2fc
maksimsavi <savianokm@gmail.com>
2020-09-29 23:59:48 -0500

    Updates

---
+++ precache-manifest.dcb3ec5896454f3687a852b85bbc1314.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af05e2e7a2f2b91bc0e95f2c6e43dd2",
+    "url": "/gtzp/index.html"
+  },
+  {
+    "revision": "32cad4b39aef0794e82e",
+    "url": "/gtzp/static/css/main.776d30d9.chunk.css"
+  },
+  {
+    "revision": "947ff92e47525e60595e",
+    "url": "/gtzp/static/js/2.bc24a3a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gtzp/static/js/2.bc24a3a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32cad4b39aef0794e82e",
+    "url": "/gtzp/static/js/main.53c9558b.chunk.js"
+  },
+  {
+    "revision": "25606ebe69febd07ffac",
+    "url": "/gtzp/static/js/runtime-main.79b54aac.js"
+  },
+  {
+    "revision": "eac4b3db44538ad8f162ac7a3b201d9e",
+    "url": "/gtzp/static/media/placeholder.eac4b3db.png"
+  },
+  {
+    "revision": "9ce190a029ebae44d72db8346a1c3e18",
+    "url": "/gtzp/static/media/zeroLogo.9ce190a0.PNG"
+  }
+]);
